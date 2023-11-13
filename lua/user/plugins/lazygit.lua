a9e6e1ae18a9857664e0191cede8c929526c824f
Valentin Berlin <valentin.berlin@protonmail.com>
2023-11-13 16:34:59 +0100

    add lazy loading for lazygit plugin

--- lua/user/plugins/lazygit.lua
+++ lua/user/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 return {
   "kdheepak/lazygit.nvim",
   -- optional for floating window border decoration
+  cmd = { "LazyGit", "LazyGitConfig" },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
