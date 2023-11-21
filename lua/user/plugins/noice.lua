2b62e2f35ca2b5faefc3985b1335feecb6aff41b
Valentin Berlin <valentin.berlin@protonmail.com>
2023-11-21 16:27:31 +0100

    add filters for noice plugin to skip some not very useful messages

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -1,23 +1,53 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      require("noice").setup({
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("noice").setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      cmdline = {
+        view = "cmdline",
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            find = "written",
           },
+          action = "close",
         },
-        cmdline = {
-          view = "cmdline",
+        {
+          filter = {
+            event = "msg_show",
+            find = "more lines",
+          },
+          action = "close",
         },
-      })
-    end
+        {
+          filter = {
+            event = "msg_show",
+            find = "fewer lines",
+          },
+          action = "close",
+        },
+        {
+          filter = {
+            event = "msg_show",
+            find = "more line",
+          },
+          action = "close",
+        },
+      },
+    })
+  end,
 }
