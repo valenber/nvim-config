62031ab387847efd555e88908220880973ca5319
Valentin Berlin <valentin.berlin@protonmail.com>
2023-12-06 20:12:37 +0100

    change chat-gpt config to use GPT-4

--- lua/user/plugins/chat-gpt.lua
+++ lua/user/plugins/chat-gpt.lua
@@ -5,6 +5,23 @@ return {
     local home = vim.fn.expand("$HOME")
     require("chatgpt").setup({
       api_key_cmd = "gpg --decrypt " .. home .. "/.config/nvim/key.gpg",
+      openai_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 300,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
     })
   end,
   dependencies = {
