c6b84d41aa647de4aa1c4bc58bba432d6f1f42cf
Valentin Berlin <valentin.berlin@protonmail.com>
2023-12-06 19:22:29 +0100

    add chat-gpt plugin

---
+++ lua/user/plugins/chat-gpt.lua
@@ -0,0 +1,15 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    local home = vim.fn.expand("$HOME")
+    require("chatgpt").setup({
+      api_key_cmd = "gpg --decrypt " .. home .. "/.config/nvim/key.gpg",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
