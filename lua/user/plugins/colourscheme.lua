ad43cf03f460f35447f301a3834734e0acec4122
Valentin Berlin <valentin.berlin@protonmail.com>
2023-12-29 09:23:13 +0100

    upate colourscheme config

--- lua/user/plugins/colourscheme.lua
+++ lua/user/plugins/colourscheme.lua
@@ -2,6 +2,14 @@ return {
   "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
+    local kanagawa = require("kanagawa")
+
+    -- why not comment something here
+    kanagawa.setup({
+      transparent = true,
+      commentStyle = { italic = true, bold = true },
+    })
+
     vim.cmd("colorscheme kanagawa-wave")
   end,
 }
